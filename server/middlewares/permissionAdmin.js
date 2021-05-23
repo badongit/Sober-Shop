143bb1f5585cb4377f80a3fefc8b16a6578a9426
unknown <nguyenbadong01@gmail.com>
2021-05-23 19:11:26 +0700

    Update middlewares permission

--- server/middlewares/permissionAdmin.js
+++
@@ -1,9 +0,0 @@
-
-const permissionAdmin = (req, res, next) => {
-    if(req.role !== 'admin')
-        return next(new ErrorResponse(400, 'No have access'));
-    
-    next();
-}
-
-module.exports = permissionAdmin;
