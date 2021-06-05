6a1e6dd5f533a0999043e2e07660552cca772bbf
unknown <nguyenbadong01@gmail.com>
2021-06-05 13:28:57 +0700

    Fix conflict

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import queryString from 'query-string'
+import axios from 'axios';
+import queryString from 'query-string';
 
 const axiosClient = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -7,14 +7,15 @@ const axiosClient = axios.create({
         'content-type': 'application/json'
     },
     paramsSerializer: params => queryString.stringify(params)
-})
+});
+
+axiosClient.interceptors.request.use(config => {
+    return config;
+});
 
 axiosClient.interceptors.response.use((response) => {
-    if (response && response.data) {
-        return response.data;
-    }
+    
     return response;
-
 }, (error) => {
     throw error;
 })
