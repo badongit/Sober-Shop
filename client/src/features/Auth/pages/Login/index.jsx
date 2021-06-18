6784d7e6cc6531123dbfc74b477bfca65587b3e7
unknown <nguyenbadong01@gmail.com>
2021-06-19 00:15:58 +0700

    Done branch auth

--- client/src/features/Auth/pages/Login/index.jsx
+++ client/src/features/Auth/pages/Login/index.jsx
@@ -1,26 +1,19 @@
-import authApi from 'api/authApi';
 import Loading from 'components/Loading/Loading';
-import { LOCAL_STORAGE } from 'constants/global';
-import { getUser } from 'features/Auth/authSlice';
 import LoginForm from 'features/Auth/components/LoginForm';
 import RegisterForm from 'features/Auth/components/RegisterForm';
 import React, { useState } from 'react';
 import { IconContext } from 'react-icons';
 import { MdClear } from 'react-icons/md';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Redirect, useHistory } from 'react-router-dom';
 import './Login.scss';
 
 function Login(props) {
     const [select, setSelect] = useState('login');
-    const dispatch = useDispatch();
     const history = useHistory();
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
     const isAuthLoading = useSelector(state => state.auth.isAuthLoading);
-
-    
    
-
     const body = isAuthLoading ? ( <div className="page-loading"><Loading backgroundColor="black" /></div>) :
          isAuthenticated ? <Redirect to='/auth/my-account' /> : 
          (
