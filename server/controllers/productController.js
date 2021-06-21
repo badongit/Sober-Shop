80881485cc2f0fdc5b28284369f476c23cb47c0b
Phương Sapphire <71723765+Phuong-Sapphire@users.noreply.github.com>
2021-06-21 21:39:28 +0700

    Update productController

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -75,4 +75,251 @@ module.exports = {
 
     return sendResponse(res, "Delete successfully.", product);
   }),
+
+  // Tý thêm xong xóa api này đi nhé
+  test: asyncHandle(async (req, res) => {
+    const product = await Product.insertMany([
+      {
+        name: "Boxy Runway T-Shirt",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-4.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-4.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-4.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-4.jpg",
+        ],
+        price: 210,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 3,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Classic Topper",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-6.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-6.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-6.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-6.jpg",
+        ],
+        price: 490,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 5,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Cotton Jacket",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-8.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-8.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3-3.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-8.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-8.jpg",
+        ],
+        price: 100,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 0,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Cotton Poplin Dress",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-9.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-9.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-9.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-9.jpg",
+        ],
+        price: 155,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 0,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Crewneck Blouse",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3-4.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1.jpg",
+        ],
+        price: 215,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 0,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Denim Dress",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-12.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3-5.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-12.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-12.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3-5.jpg",
+        ],
+        price: 90,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 3,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Flared Skirts",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-2.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1-2.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-2.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1-2.jpg",
+        ],
+        price: 90,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 5,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Light Felt Coat",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-6.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3_1-3.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1-6.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-6.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3_1-3.jpg",
+        ],
+        price: 430,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 4,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Medium Sleeve Dress",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-22.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-22.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-22.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-22.jpg",
+        ],
+        price: 140,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 4,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Over-Sized Pants",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-25.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-25.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-25.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-25.jpg",
+        ],
+        price: 105,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 2,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Runway Pants",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-28.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-28.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/3-18.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-28.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-28.jpg",
+        ],
+        price: 40,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 2,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Sleeveless Dress",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-8.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1-8.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1_1-8.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2_1-8.jpg",
+        ],
+        price: 160,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 3,
+        category: "60afea098841df28fc60331b",
+      },
+      {
+        name: "Stripe Sweater",
+        thumb: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-33.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-33.jpg",
+        ],
+        listImage: [
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/1-33.jpg",
+          "https://demo.uix.store/sober/wp-content/uploads/sites/2/2016/07/2-33.jpg",
+        ],
+        price: 200,
+        discount: 0,
+        evaluation: 0,
+        description:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        sold: 3,
+        category: "60afea098841df28fc60331b",
+      },
+    ]);
+
+    return sendResponse(res, "Store successfully.", product);
+  }),
 };
