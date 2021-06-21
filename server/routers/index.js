839ea77e7021bf2af1c6e393bb01d69bef82d773
Phương Sapphire <71723765+Phuong-Sapphire@users.noreply.github.com>
2021-06-21 21:46:51 +0700

    Revert "Favourite API"

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,10 +1,9 @@
 const authRouter = require("./auth");
-const cartRouter = require("./cart");
-const orderRouter = require("./order");
+const cartRouter = require('./cart');
+const orderRouter = require('./order');
 const productRouter = require("./product");
 const categoryRouter = require("./category");
 const feedbackRouter = require("./feedback");
-const favouriteProductRouter = require("./favouriteProduct");
 const errorHandle = require("../middlewares/errorHandle");
 
 module.exports = (app) => {
@@ -14,13 +13,11 @@ module.exports = (app) => {
 
   app.use("/api/category", categoryRouter);
 
-  app.use("/api/cart", cartRouter);
+  app.use('/api/cart', cartRouter);
 
-  app.use("/api/order", orderRouter);
+  app.use('/api/order', orderRouter);
 
-  app.use("/api/feedback", feedbackRouter);
-
-  app.use("/api/favouriteProduct", favouriteProductRouter);
+  app.use('/api/feedback', feedbackRouter);
 
   app.use(errorHandle);
 };
